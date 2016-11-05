124a837cdb9f9b60570a0ca5280d7e772c760df4
yutakakn <yutakakn@gmail.com>
2016-11-06 01:03:45 +0900

    Add profile for PowerShell

---
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,106 @@
+<#
+PowerShell用プロファイル設定 for Windows10(PowerShell 5.1)
+
+PS>notepad $profile
+
+(C) 2016 Yutaka Hirata
+https://github.com/yutakakn
+#>
+
+# コマンドラインの操作モードをEmacs系にする・ベル音を消す
+Set-PSReadlineOption -EditMode Emacs -BellStyle None
+
+# カレントディレクトリの設定
+Set-Location C:\usr\GitHub\MyScript\PowerShell
+
+# タイトルを設定する
+$a = (Get-Host).UI.RawUI 
+$add_str = " - Yutaka Hirata"
+if (! $a.WindowTitle.Contains($add_str)) {
+   $a.WindowTitle += $add_str
+}
+
+#
+# F2キーを押すと、行全体をクリップボードへコピーする。
+#
+Set-PSReadlineKeyHandler -Chord F2 `
+                         -BriefDescription SelectEntireCommandLine `
+                         -Description "Selects the entire command line" `
+                         -ScriptBlock {
+    param($key, $arg)
+  
+    [Microsoft.PowerShell.PSConsoleReadLine]::BeginningOfLine($key, $arg)
+  
+    $line = $null
+    $cursor = $null
+    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+
+    # 選択状態にする  
+    while ($cursor -lt $line.Length) {
+        [Microsoft.PowerShell.PSConsoleReadLine]::SelectForwardChar($key, $arg)
+        [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+    }
+
+    # クリップボードへ送信する
+    [Microsoft.PowerShell.PSConsoleReadLine]::CopyOrCancelLine($key, $arg)
+}
+
+#
+# F7キーを押すと、下記ヒストリファイルから過去の履歴を表示および選択可能になる。
+# 
+# (get-psreadlineoption).HistorySavePath
+# C:\Users\yutaka\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadline\ConsoleHost_history.txt
+#
+Set-PSReadlineKeyHandler -Key F7 `
+                         -BriefDescription History `
+                         -LongDescription 'Show command history' `
+                         -ScriptBlock {
+    $pattern = $null
+    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$pattern, [ref]$null)
+    if ($pattern)
+    {
+        $pattern = [regex]::Escape($pattern)
+    }
+
+    $history = [System.Collections.ArrayList]@(
+        $last = ''
+        $lines = ''
+        foreach ($line in [System.IO.File]::ReadLines((Get-PSReadlineOption).HistorySavePath))
+        {
+            if ($line.EndsWith('`'))
+            {
+                $line = $line.Substring(0, $line.Length - 1)
+                $lines = if ($lines)
+                {
+                    "$lines`n$line"
+                }
+                else
+                {
+                    $line
+                }
+                continue
+            }
+
+            if ($lines)
+            {
+                $line = "$lines`n$line"
+                $lines = ''
+            }
+
+            if (($line -cne $last) -and (!$pattern -or ($line -match $pattern)))
+            {
+                $last = $line
+                $line
+            }
+        }
+    )
+    $history.Reverse()
+
+    $command = $history | Out-GridView -Title History -PassThru
+    if ($command)
+    {
+        [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
+        [Microsoft.PowerShell.PSConsoleReadLine]::Insert(($command -join "`n"))
+    }
+}
+
