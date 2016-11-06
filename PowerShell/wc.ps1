ed4061724c4c0b88c1f180bc650644e0fc4d31b7
yutakakn <yutakakn@gmail.com>
2016-11-07 00:05:15 +0900

    Linuxのwcコマンドと同じオプション(-l, -c, -w)を追加した。 ひとまず、これで完成形とする。

--- PowerShell/wc.ps1
+++ PowerShell/wc.ps1
@@ -1,45 +1,109 @@
 <#
-テキストファイルの文字数(改行を除く)をカウントする
+テキストファイルの行数・文字数・単語数をカウントする
 
 実行例:
-PS C:\Users\yutaka> C:\usr\GitHub\MyScript\PowerShell\wc.ps1 C:\sample\付録.txt
-Characters : 53409
+PS C:\usr\GitHub\MyScript\PowerShell> ./wc.ps1 -Encoding SJIS C:\usr\doc\sample.txt
+行数(空白行を除く)は下記のとおりです
+Lines : 21
+文字数(改行を除く)は下記のとおりです
+Characters : 548
+単語数は下記のとおりです
+Words : 28
 
 (C) 2016 Yutaka Hirata
+https://github.com/yutakakn
 #>
 
-function PrintWordCountFile($filename) {
-#    Write-Output "Filename: " $filename
+# コマンドラインオプションの定義
+Param(
+    [switch]$c,
+    [switch]$l,
+    [switch]$w,
+    [ValidateSet("Default", "SJIS", "UTF8")]$Encoding,
+    [string]$Filename
+)
 
-    # SJISファイルを読み込む
-    $local:file = Get-Content -Encoding Default $filename
+function GetLineCountFile() {
+    PARAM($parm)
+    $filename = [String]$parm[0]
+    $enc = [String]$parm[1]
+    $file = Get-Content -Encoding $enc -Path $filename
 
-#    Write-Output "Linesは行数, Charactersは文字数(≠バイト数), Wordsは単語数" 
-#                 "行数には空白行を含まない" 
-#                 "文字数には改行は含まない"
-#    $file | Measure-Object -Line -Character -Word | Format-List Lines,Characters,Words
-     $file | Measure-Object -Line -Character -Word | Format-List Characters
-
-#    Write-Output "空白行を含む行数"
-#    $file | Measure-Object | Format-List Count
+    Write-Output "行数(空白行を除く)は下記のとおりです"
+     $file | Measure-Object -Line -Character -Word | Format-List Lines
 }
 
-# main routine
+function GetCharacterCountFile() {
+    PARAM($parm)
+    $filename = [String]$parm[0]
+    $enc = [String]$parm[1]
+    $file = Get-Content -Encoding $enc -Path $filename
 
+    Write-Output "文字数(改行を除く)は下記のとおりです"
+     $file | Measure-Object -Line -Character -Word | Format-List Characters
+}
+
+function GetWordCountFile() {
+    PARAM($parm)
+    $filename = [String]$parm[0]
+    $enc = [String]$parm[1]
+    $file = Get-Content -Encoding $enc -Path $filename
+
+    Write-Output "単語数は下記のとおりです"
+     $file | Measure-Object -Line -Character -Word | Format-List Words
+}
+
+# コマンドラインヘルプ
 $helpmsg = @'
-Help: Word count program"
-wc.ps1 <text file1> [<text file2> .. ]
+Usage: wc.ps1 [OPTION] <FILE>
+Word count program prints newline, word, and byte counts for each FILE.
+FILE can accept wildcard character.
+
+  -c   print byte counts
+  -l   print newline counts
+  -w   print word counts
+  -Encoding  SJIS|UTF8
+      specify encoding character
 
 Example:
-wc.ps1 test.txt
-wc.ps1 *.txt
+./wc.ps1 -Encoding SJIS -Filename test.txt
+./wc.ps1 -l -Encoding SJIS test.txt
+./wc.ps1 -c -Encoding SJIS *.txt
 '@
 
-if ($args.Length -eq 0) {
+# ファイル名の指定がなければ、コマンドラインヘルプを表示する。
+if ([String]::IsNullOrEmpty($Filename)) {
     Write-Output($helpmsg)
     exit
 }
 
-for ($i = 0 ; $i -lt $args.Length ; $i++) {
-    PrintWordCountFile($args[$i])
+if ([String]::IsNullOrEmpty($Encoding)) {
+    $Encoding = "Default"
+}
+if ($Encoding -eq "SJIS") {
+    $Encoding = "Default"
+}
+
+#echo "[$Filename]"
+#echo "[$Encoding]"
+
+$n = 0
+if ($l) {
+    GetLineCountFile @($Filename, $Encoding)
+    $n++
+}
+if ($c) {
+    GetCharacterCountFile @($Filename, $Encoding)
+    $n++
+}
+if ($w) {
+    GetWordCountFile @($Filename, $Encoding)
+    $n++
+}
+
+# すべてのオプションが未指定の場合、全表示する。
+if ($n -eq 0) {
+    GetLineCountFile @($Filename, $Encoding)
+    GetCharacterCountFile @($Filename, $Encoding)
+    GetWordCountFile @($Filename, $Encoding)
 }
