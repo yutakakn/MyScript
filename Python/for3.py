2de852cd735f305d438b4a7f3068d47a8486925c
ymko <ymko@ymko.com>
2017-07-20 00:51:57 +0900

    pycharmのre-format、inspection機能で自分なりに変更

--- Python/for3.py
+++ Python/for3.py
@@ -1,19 +1,30 @@
 # coding: UTF-8
-# 九九を求める
 
-def kuku(MaxValue):
-	max = MaxValue + 1
-	val = 0
-	for i in range(1, max):
-		for j in range(1, max):
-			val = i * j
-			print('%3d') % (val),
-		print('')
+from __future__ import unicode_literals, print_function, division, absolute_import
 
-print('九九を求める -日本式-')
-kuku(9)
 
-print('九九を求める -インド式-')
-kuku(20)
+def main():
+    # 九九を求める
 
+    def kuku(MaxValue):
+
+        i_max = MaxValue + 1
+        # val = 0
+        for i in range(1, i_max):
+            for j in range(1, i_max):
+                val = i * j
+                # print('%3d') % (val),
+                print("{0:3} ".format(val), end='')
+            print('')
+
+    print('九九を求める -日本式-')
+    kuku(9)
+
+    print('九九を求める -インド式-')
+    kuku(20)
+
+
+if __name__ == '__main__':
+    main()
+    exit(0)
 
